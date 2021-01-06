4911f3022a37a25ce08acca8d608a48b2446eaf5
jaywcjlove <398188662@qq.com>
2021-01-06 20:03:12 +0800

    refactor: rebuild code.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
 const github = require('@actions/github');
 const regexp = /^[.A-Za-z0-9_-]*$/;
 
@@ -18,6 +17,8 @@ async function run() {
     var headRef = core.getInput('head-ref');
     var baseRef = core.getInput('base-ref');
     const myToken = core.getInput('token');
+    const filterAuthor = core.getInput('filter-author');
+    const regExp = core.getInput('filter');
     const { owner, repo } = github.context.repo;
     const octokit = github.getOctokit(myToken);
 
@@ -49,92 +50,41 @@ async function run() {
         head: headRef,
       });
 
-      if (commits.status !== 200) {
+      if (commits && commits.status !== 200) {
         core.setFailed(
           `There are no releases on ${owner}/${repo}. Tags are not releases. (status=${commits.status}) ${commits.data.message || ''}`
         );
       }
-
+      let changelog = '';
       for (const data of commits.data.commits) {
-        core.info(`Commit: \x1b[34m${data.commit.message}\x1b[0m \x1b[34m${data.commit.message}\x1b[0m ${data.commit.author.name} ${data.sha}`)
+        core.info(`Commit: \x1b[34m${data.commit.message}\x1b[0m \x1b[34m${data.author.name}\x1b[0m ${data.sha}`);
+        changelog += formatStringCommit(data.commit.message, `${owner}/${repo}`, {
+          regExp, shortHash: data.sha, filterAuthor, hash: data.sha, author: data.author.name
+        });
       }
-
-      // By default a GitHub action checkout is shallow. Get all the tags, branches,
-      // and history. Redirect output to standard error which we can collect in the
-      // action.
-      await exec.exec('git fetch --depth=1 origin +refs/tags/*:refs/tags/*');
-      await exec.exec('git fetch --prune --unshallow');
-
-      // const commitList = await octokit.repos.listBranchesForHeadCommit({
-      //   ...github.context.repo,
-      //   commit_sha: baseHash || baseRef
-      // })
-      // core.info(`Commit List: \x1b[34m${JSON.stringify(commitList.data)}\x1b[0m`)
-      // octokit.repos.listCommits({})
-      // octokit.repos.listCommentsForCommit
-      // octokit.repos.listBranchesForHeadCommit
 
       let tagRef = '';
       if ((github.context.ref || '').startsWith('refs/tags/')) {
-        tagRef = getVersion(github.context.ref)
-        core.info(`Tag: \x1b[34m${tagRef}\x1b[0m`)
+        tagRef = getVersion(github.context.ref);
+        core.info(`Tag: \x1b[34m${tagRef}\x1b[0m`);
         core.setOutput('tag', tagRef);
       }
-
       if ((github.context.ref || '').startsWith('refs/heads/')) {
         const branch = github.context.ref.replace(/.*(?=\/)\//, '');
         core.setOutput('branch', branch);
-        core.info(`Branch: \x1b[34m${branch}\x1b[0m`)
+        core.info(`Branch: \x1b[34m${branch}\x1b[0m`);
       }
-
-      core.info(`Tag: \x1b[34m${tagRef}\x1b[0m`)
-      core.info(`Input head-ref: \x1b[34m${headRef}\x1b[0m`)
-      core.info(`Input base-ref: \x1b[34m${baseRef}\x1b[0m`)
-      getChangelog(headRef, baseRef, { repoName: owner + '/' + repo, tagRef });
+      core.info(`Tag: \x1b[34m${tagRef || '-'}\x1b[0m`);
+      core.info(`Input head-ref: \x1b[34m${headRef}\x1b[0m`);
+      core.info(`Input base-ref: \x1b[34m${baseRef}\x1b[0m`);
+      core.setOutput('compareurl', `https://github.com/${owner}/${repo}/compare/${baseRef}...${tagRef || headRef}`);
+      core.setOutput('changelog', changelog);
     } else {
       core.setFailed(
         'Branch names must contain only numbers, strings, underscores, periods, and dashes.'
       );
     }
 
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-async function getChangelog(headRef, baseRef, { repoName, tagRef }) {
-  try {
-    let output = ''
-    let err = ''
-
-    // These are option configurations for the @actions/exec lib`
-    await exec.exec(
-      `git log "${baseRef}...${headRef}" --pretty="format:[,,,]%h[,,,]%H[,,,]%s[,,,]%an" --reverse`,
-      [],
-      {
-        cwd: './',
-        listeners: {
-          stdout: data => {
-            output += data.toString();
-          },
-          stderr: data => {
-            err += data.toString();
-          }
-        }
-      }
-    );
-
-    if (output) {
-      const regExp = core.getInput('filter');
-      const changelog = formatString(output, repoName, { regExp });
-      console.log('\x1b[32m%s\x1b[0m', `Changelog between ${baseRef} and ${headRef}:\n${changelog}`);
-      core.setOutput('compareurl', `https://github.com/${repoName}/compare/${baseRef}...${tagRef || headRef}`);
-      core.setOutput('changelog', changelog);
-    } else {
-      core.setFailed(err);
-      process.exit(1);
-    }
-    
   } catch (error) {
     core.setFailed(
       `Could not generate changelog between references because: ${error.message}`
@@ -143,57 +93,39 @@ async function getChangelog(headRef, baseRef, { repoName, tagRef }) {
   }
 }
 
-/**
- * `%h[,,,]%H[,,,]%s[,,,]%an[-|-]`
- * @param {*} str ``
- * @param {*} repoName `uiwjs/uiw`
- * @param {*} regExp `^released`
- */
-function formatString(str = '', repoName = '', { regExp }) {
-  const filterAuthor = core.getInput('filter-author');
-  let result = '';
-  str.split('\n').filter(Boolean).forEach((subStr) => {
-    const strArr = subStr.split('[,,,]');
-    const shortHash = strArr[1];
-    const hash = strArr[2];
-    let commit = strArr[3];
-    let author = strArr[4];
-    if ((new RegExp(filterAuthor)).test(author) || filterAuthor === false) {
-      author = '';
-    }
-    if (regExp && (new RegExp(regExp).test(commit))) {
-      return;
-    }
-
-    if (getRegExp('type', commit)) {
-      commit = `🆎 ${commit}`;
-    } else if (getRegExp('feat', commit)) {
-      commit = `🌟 ${commit}`;
-    } else if (getRegExp('style', commit)) {
-      commit = `🎨 ${commit}`;
-    } else if (getRegExp('chore', commit)) {
-      commit = `💄 ${commit}`;
-    } else if (getRegExp('doc', commit) || getRegExp('docs', commit)) {
-      commit = `📖 ${commit}`;
-    } else if (getRegExp('fix', commit)) {
-      commit = `🐞 ${commit}`;
-    } else if (getRegExp('test', commit)) {
-      commit = `⛑ ${commit}`;
-    } else if (getRegExp('refactor', commit)) {
-      commit = `🐝 ${commit}`;
-    } else if (getRegExp('website', commit)) {
-      commit = `🌍 ${commit}`;
-    } else if (getRegExp('revert', commit)) {
-      commit = `🔙 ${commit}`;
-    } else if (getRegExp('clean', commit)) {
-      commit = `💊 ${commit}`;
-    } else {
-      commit = `📄 ${commit}`;
-    }
-    const changelog = `- ${commit} [\`${shortHash}\`](http://github.com/${repoName}/commit/${hash})${author ?` @${author}`: ''}`;
-    result += `${changelog}\n`;
-  });
-  return result;
+function formatStringCommit(commit = '', repoName = '', { regExp, shortHash, filterAuthor, hash, author }) {
+  if ((new RegExp(filterAuthor)).test(author) || filterAuthor === false) {
+    author = '';
+  }
+  if (regExp && (new RegExp(regExp).test(commit))) {
+    return;
+  }
+  if (getRegExp('type', commit)) {
+    commit = `🆎 ${commit}`;
+  } else if (getRegExp('feat', commit)) {
+    commit = `🌟 ${commit}`;
+  } else if (getRegExp('style', commit)) {
+    commit = `🎨 ${commit}`;
+  } else if (getRegExp('chore', commit)) {
+    commit = `💄 ${commit}`;
+  } else if (getRegExp('doc', commit) || getRegExp('docs', commit)) {
+    commit = `📖 ${commit}`;
+  } else if (getRegExp('fix', commit) || getRegExp('fixed', commit)) {
+    commit = `🐞 ${commit}`;
+  } else if (getRegExp('test', commit)) {
+    commit = `⛑ ${commit}`;
+  } else if (getRegExp('refactor', commit)) {
+    commit = `🐝 ${commit}`;
+  } else if (getRegExp('website', commit)) {
+    commit = `🌍 ${commit}`;
+  } else if (getRegExp('revert', commit)) {
+    commit = `🔙 ${commit}`;
+  } else if (getRegExp('clean', commit)) {
+    commit = `💊 ${commit}`;
+  } else {
+    commit = `📄 ${commit}`;
+  }
+  return `- ${commit} [\`${shortHash}\`](http://github.com/${repoName}/commit/${hash})${author ?` @${author}`: ''}\n`;
 }
 
 function getRegExp(str = '', commit = '') {
