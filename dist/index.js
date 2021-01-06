d14930f98c3be34e7bdb0c48a5f385d977e0e6a0
jaywcjlove <398188662@qq.com>
2021-01-06 20:47:26 +0800

    chore: Add result log.

--- dist/index.js
+++ dist/index.js
@@ -64,7 +64,6 @@ async function run() {
       }
       let changelog = '';
       for (const data of commits.data.commits) {
-        core.info(`Commit Data: \x1b[34m${JSON.stringify(data)}\x1b[0m`);
         core.info(`Commit: \x1b[34m${data.commit.message}\x1b[0m \x1b[34m${data.commit.committer.name}\x1b[0m ${data.sha}`);
         changelog += formatStringCommit(data.commit.message, `${owner}/${repo}`, {
           regExp, shortHash: data.sha.slice(0, 7), filterAuthor, hash: data.sha, author: data.commit.committer.name
@@ -85,8 +84,8 @@ async function run() {
       core.info(`Tag: \x1b[34m${tagRef || '-'}\x1b[0m`);
       core.info(`Input head-ref: \x1b[34m${headRef}\x1b[0m`);
       core.info(`Input base-ref: \x1b[34m${baseRef}\x1b[0m`);
-      core.group('Result Changelog');
-      console.log(`changelog`);
+      core.startGroup('Result Changelog');
+      console.log(`${changelog}`);
       core.endGroup();
       core.setOutput('compareurl', `https://github.com/${owner}/${repo}/compare/${baseRef}...${tagRef || headRef}`);
       core.setOutput('changelog', changelog);
