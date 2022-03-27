0052daaed8a67a8cf9b5401d76da4161c1524444
jaywcjlove <398188662@qq.com>
2022-03-27 20:59:15 +0800

    released v1.5.1

--- dist/index.js
+++ dist/index.js
@@ -11901,8 +11901,6 @@ try {
 }
 })();
 
-var __webpack_export_target__ = exports;
-for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
-if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+module.exports = __webpack_exports__;
 /******/ })()
 ;
